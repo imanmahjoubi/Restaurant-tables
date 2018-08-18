5ee9823361b296dd26b947ea04861990823fdc41
Imen Mahjoubi <imen.mahjoubi@esprit.tn>
2018-08-18 11:58:35 -0700

    server and page 1

--- server.js
+++ server.js
@@ -8,8 +8,21 @@ var bodyParser = require("body-parser");
 
 var PORT = 3010;
 
+var available_tables = [
+    {
+        "name" : "Imen",
+        "Phone" : "4242424242",
+        "Email" : "imen@gmail.com",
+        "unique_id" : "1233"
+    },
+    {
+        "name" : "alex",
+        "Phone" : "4242111142",
+        "Email" : "alex@gmail.com",
+        "unique_id" : "2222"
+    }
 
-var available_tables = [];
+];
 
 var waiting_list = [];
 
@@ -30,9 +43,39 @@ app.listen(PORT, function() {
 });
 
 app.get("/api/tables", function(req, res) {
-    return res.json(tables.json);
+    return res.json(available_tables);
+    console.log(available_tables);
 
 });
+app.get("/api/tables/:table", function(req, res) {
+    var chosen = req.params.table;
+
+    console.log(chosen);
+
+    for (var i = 0; i < available_tables.length; i++) {
+        if (chosen === available_tables[i].unique_id) {
+            return res.json(available_tables[i]);
+        }
+    }
+
+    return res.json(false);
+});
+
+app.post("/api/tables", function(req, res) {
+
+    var newtable = req.body;
+    console.log(newtable);
+    available_tables.push(newtable);
+    res.json(newtable);
+});
+
+
+
+
+
+
+
+
 
 
 
